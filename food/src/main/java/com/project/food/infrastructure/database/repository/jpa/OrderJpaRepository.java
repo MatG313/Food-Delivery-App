5ea42205d0c41bcc7e98ca8adf2ed51ca537e779
Mateusz Gajewski <mateo31313@gmail.com>
2023-09-21 12:52:06 +0200

    finishing creating everything with database

--- food/src/main/java/com/project/food/infrastructure/database/repository/jpa/OrderJpaRepository.java
+++ food/src/main/java/com/project/food/infrastructure/database/repository/jpa/OrderJpaRepository.java
@@ -1,4 +1,25 @@
 package com.project.food.infrastructure.database.repository.jpa;
 
-public interface OrderJpaRepository {
+import com.project.food.infrastructure.database.entity.OrderEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
+
+    @Query("SELECT d FROM OrderEntity d WHERE d.restaurantId.restaurantId = :restaurantId")
+    List<OrderEntity> findByRestaurantId(@Param("restaurantId") Long restaurantId);
+    @Query("SELECT d FROM OrderEntity d WHERE d.userId.userId = :userId")
+    List<OrderEntity> findByUserId(@Param("userId") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE OrderEntity SET status= :status WHERE orderId = :id")
+    void toggleStatus(@Param("id") Long id,@Param("status") String status);
 }
--- food/src/main/java/com/project/food/infrastructure/database/repository/jpa/UserJpaRepository.java
+++ food/src/main/java/com/project/food/infrastructure/database/repository/jpa/UserJpaRepository.java
@@ -1,4 +1,19 @@
 package com.project.food.infrastructure.database.repository.jpa;
 
-public interface UserJpaRepository {
+import com.project.food.infrastructure.database.entity.RestaurantEntity;
+import com.project.food.infrastructure.database.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<UserEntity, Integer> {
+
+    UserEntity findByEmail(String email);
+    List<UserEntity> findByRole(Integer role);
+    @Query("SELECT u.restaurants FROM UserEntity u WHERE u.userId = :userId")
+    List<RestaurantEntity> findRestaurantsByUserId(@Param("userId") Long userId);
 }
