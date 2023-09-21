58f451046b2439937d73277e93d24291189870e1
Mateusz Gajewski <mateo31313@gmail.com>
2023-09-21 20:21:02 +0200

    adding swagger documentation

--- food/src/main/java/com/project/food/configuration/SpringSecurity.java
+++ food/src/main/java/com/project/food/configuration/SpringSecurity.java
@@ -4,6 +4,7 @@ import com.project.food.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,6 +39,7 @@ public class SpringSecurity {
                                 .requestMatchers("/swagger-ui").permitAll()
                                 .requestMatchers("/api-docs/**").permitAll()
                                 .requestMatchers("/index").permitAll()
+                                .requestMatchers(HttpMethod.GET).permitAll()
                                 .requestMatchers("/dashboard").hasAnyRole("ADMIN", "OWNER", "USER")
                                 .requestMatchers("/restaurant-dashboard").hasAnyRole("ADMIN", "OWNER", "USER")
                                 .requestMatchers("/restaurant-owner").hasAnyRole("ADMIN", "OWNER")
